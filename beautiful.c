1f09dd4415ea5590e5500094b1c2ac4747bf8e53
llibedojo <ojodebill@gmail.com>
2024-03-06 12:51:48 +0300

    code

---
+++ beautiful.c
@@ -0,0 +1,26 @@
+/*
+ * File: beautiful_code.c
+ * Author: Bill Ojode
+ * Description: A beautiful code example that passes the Betty style checker.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int num1 = 10;
+    int num2 = 20;
+    int sum;
+
+    sum = num1 + num2;
+
+    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+
+    return (0);
+}
+
